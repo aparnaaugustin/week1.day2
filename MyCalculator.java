827f7eb6d1b89b2aa267ca4d927046ebe369c129
aparnaaugustin <60378430+aparnaaugustin@users.noreply.github.com>
2021-08-09 12:09:49 +0530

    Add files via upload

---
+++ MyCalculator.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class MyCalculator {
+
+	public static void main(String[] args) {
+		Calculator obj=new Calculator();
+		System.out.println(obj.add(3, 4, 5));;
+		System.out.println(obj.sub(5, 4));
+		System.out.println(obj.mul(156.0000, 1.0000));
+		System.out.println(obj.divide(1.6f, 0.2f));
+
+	}
+
+}
