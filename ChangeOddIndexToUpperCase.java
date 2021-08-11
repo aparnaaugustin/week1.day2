3bc32789799937b894032c39490e3e212dbf9199
aparnaaugustin <60378430+aparnaaugustin@users.noreply.github.com>
2021-08-11 08:47:51 +0530

    Add files via upload

---
+++ ChangeOddIndexToUpperCase.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class ChangeOddIndexToUpperCase {
+
+	public static void main(String[] args) {
+		String test = "changeme";
+		char[]arr=test.toCharArray();
+		for (int i = 0; i< arr.length; i++) {
+			if(i%2!=0)
+			{
+				System.out.println(Character.toUpperCase(arr[i]));
+			}
+		}
+
+	}
+
+}
